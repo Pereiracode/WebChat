ef43973c62c2963b4a06067bb9005983b6a6c796
edluz <edson.caluz@outlook.com>
2021-05-04 18:12:50 -0300

    Adicionado o serviço ProfanityFilterService

---
+++ WebChat/WebChat.Services/Interfaces/IProfanityFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebChat.Services.Interfaces
+{
+    public interface IProfanityFilter
+    {
+        public string Filter(string text);
+    }
+}
